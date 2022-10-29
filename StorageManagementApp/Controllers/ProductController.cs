abcb60becb503fdc0ecb254fc50c6cc4706d42fc
Anastasija <anastasija.savov2000@gmail.com>
2022-10-29 17:37:24 +0200

    missing files to commit

--- StorageManagementApp/Controllers/ProductController.cs
+++ StorageManagementApp/Controllers/ProductController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StorageManagementApp.Contracts.DTOs.Product;
+using StorageManagementApp.Mvc.Services.Interfaces;
 
 namespace StorageManagementApp.Mvc.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
         {
-            return View();
+            _productService = productService;
         }
+        [HttpPost]
+        public IActionResult Create(ProductCreateDto product)
+        {
+            var res = _productService.AddProduct(product);
+
+            if (res) return View(); //with toast success
+            else return View(); //with error message
+        }
+
     }
 }
